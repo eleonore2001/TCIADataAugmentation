dca44ff364bd96199aa51a6aa793b8705be65e02
eleonore2001 <elegermond@gmail.com>
2025-08-08 13:28:34 +0000

    Update TCIA_data_augmentation.sh
    Creates a loop that creates deformation for each scan in 6 different axis.

--- TCIA_data_augmentation.sh
+++ TCIA_data_augmentation.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-# Path to your Slicer executable
 SLICER_PATH="/opt/Slicer-5.9.0-2025-06-27-linux-amd64/Slicer"
 
-# Path to your Python script
-PYTHON_SCRIPT="/home/eleonore/TCIADataAugmentation/scripts/data_eleonore.py"
+# Define orientation and direction arrays (Bash syntax)
+L_orientation=("x" "y" "z")
+L_direction=("up" "down")
 
 # Loop from 1 to 199
 for i in {1..199}; do
     echo "Processing file index: $i"
-    
-    #  $SLICER_PATH --no-main-window --exit-after-startup \
-    #     --python-code "i=$i; exec(open('/home/eleonore/TCIADataAugmentation/scripts/data_eleonore.py').read())"
 
-    $SLICER_PATH --exit-after-startup \  
-        --python-code "i=$i; exec(open('/home/eleonore/TCIADataAugmentation/scripts/data_eleonore.py').read())"
-    
-    
+    for orientation in "${L_orientation[@]}"; do
+        for direction in "${L_direction[@]}"; do
+            echo " -> orientation=$orientation, direction=$direction"
+            
+            $SLICER_PATH --exit-after-startup \
+                --python-code "i=$i; orientation='$orientation'; direction='$direction'; exec(open('/home/eleonore/TCIADataAugmentation/scripts/TCIA_data_augmentation.py').read())"
+        done
+    done
+
     echo "Completed processing for index $i"
 done
-
