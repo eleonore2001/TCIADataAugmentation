43654c41365a353b7d1cc445a2fee8e1a3c330a5
eleonore2001 <elegermond@gmail.com>
2025-08-13 09:21:38 +0000

    Update TCIA_data_augmentation.sh

--- TCIA_data_augmentation.sh
+++ TCIA_data_augmentation.sh
@@ -5,6 +5,8 @@ SLICER_PATH="/opt/Slicer-5.9.0-2025-06-27-linux-amd64/Slicer"
 # Define orientation and direction arrays (Bash syntax)
 L_orientation=("x" "y" "z")
 L_direction=("up" "down")
+magnitude=700
+duration=7
 
 # Loop from 1 to 198
 for i in {1..198}; do
@@ -15,13 +17,21 @@ for i in {1..198}; do
         for direction in "${L_direction[@]}"; do
             echo " -> orientation=$orientation, direction=$direction, first_time=$first_time"
             
-            $SLICER_PATH --exit-after-startup \
-                --python-code "i=$i; orientation='$orientation'; direction='$direction'; first_time=$first_time; exec(open('/home/eleonore/TCIADataAugmentation/scripts/TCIA_data_augmentation.py').read())"
+               $SLICER_PATH --exit-after-startup \
+                --python-code "
+i=$i; 
+magnitude=$magnitude; 
+duration=$duration; 
+orientation='$orientation'; 
+direction='$direction'; 
+first_time=$first_time; 
+exec(open('/home/eleonore/TCIADataAugmentation/scripts/TCIA_data_augmentation.py').read())"
             first_time=False
         done
     done
 
     echo "Completed processing for index $i"
 done
+
 
 
